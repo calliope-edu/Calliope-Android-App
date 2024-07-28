0052ee7875e388d5563073ccc3f2b8febca7bf3f
Tsisar <fhantom.tp@gmail.com>
2024-07-28 18:02:09 +0300

    Refactoring WebInfoFragment

--- app/src/main/java/cc/calliope/mini/WebInfoFragment.kt
+++ app/src/main/java/cc/calliope/mini/WebInfoFragment.kt
@@ -1,5 +1,7 @@
 package cc.calliope.mini
 
+import android.app.UiModeManager
+import android.content.Context
 import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,8 +10,6 @@ import android.view.ViewGroup
 import android.webkit.WebView
 import androidx.fragment.app.Fragment
 import java.util.Locale
-import android.app.UiModeManager
-import android.content.Context
 
 class WebInfoFragment : Fragment() {
     companion object {
@@ -25,15 +25,14 @@ class WebInfoFragment : Fragment() {
 
         // Get the current language of the device
         val locale = Locale.getDefault()
-        val language = locale.language // ISO language code, e.g., "en", "ua", etc.
-
-        // Detect the current theme (dark or light)
-        val uiModeManager = requireContext().getSystemService(Context.UI_MODE_SERVICE) as UiModeManager
-        val currentMode = uiModeManager.currentModeType
-        val theme = if (currentMode == Configuration.UI_MODE_NIGHT_YES) "dark" else "light"
+        val language = when(locale.language) {
+            "de" -> "de" // German
+            "uk" -> "uk" // Ukrainian
+            else -> ""
+        }
 
         // Add the locale and theme parameters to the URL
-        val urlWithLocaleAndTheme = "https://app.calliope.cc/android/?locale=$language&theme=$theme"
+        val urlWithLocaleAndTheme = "https://app.calliope.cc/android/$language"
 
         webView.loadUrl(urlWithLocaleAndTheme)
         return view
